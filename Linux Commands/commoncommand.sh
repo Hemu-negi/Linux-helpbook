bc201f242b01f9ad82d3fa087f9e2d6f36ed6a44
Hemu-negi <Hemusingh333@gmail.com>
2024-12-29 01:45:17 +0530

    Linux Commands

---
+++ Linux Commands/commoncommand.sh
@@ -0,0 +1,62 @@
+pwd                       # Print current directory path
+ls                        # List directories
+ls -a|--all               # List directories including hidden
+ls -l                     # List directories in long form
+ls -l -h|--human-readable # List directories in long form with human readable sizes
+ls -t                     # List directories by modification time, newest first
+stat foo.txt              # List size, created and modified timestamps for a file
+stat foo                  # List size, created and modified timestamps for a directory
+tree                      # List directory and file tree
+tree -a                   # List directory and file tree including hidden
+tree -d                   # List directory tree
+cd foo                    # Go to foo sub-directory
+cd                        # Go to home directory
+cd ~                      # Go to home directory
+cd -                      # Go to last directory
+pushd foo                 # Go to foo sub-directory and add previous directory to stack
+popd                      # Go back to directory in stack saved by `pushd`
+
+mkdir foo                        # Create a directory
+mkdir foo bar                    # Create multiple directories
+mkdir -p|--parents foo/bar       # Create nested directory
+mkdir -p|--parents {foo,bar}/baz # Create multiple nested directories
+
+mktemp -d|--directory            # Create a temporary directory
+
+
+cp -R|--recursive foo bar                               # Copy directory
+mv foo bar                                              # Move directory
+
+rsync -z|--compress -v|--verbose /foo /bar              # Copy directory, overwrites destination
+rsync -a|--archive -z|--compress -v|--verbose /foo /bar # Copy directory, without overwriting destination
+rsync -avz /foo username@hostname:/bar                  # Copy local directory to remote directory
+rsync -avz username@hostname:/foo /bar                  # Copy remote directory to local directory
+
+rmdir foo                        # Delete empty directory
+rm -r|--recursive foo            # Delete directory including contents
+rm -r|--recursive -f|--force foo # Delete directory including contents, ignore nonexistent files and never prompt
+
+touch foo.txt          # Create file or update existing files modified timestamp
+touch foo.txt bar.txt  # Create multiple files
+touch {foo,bar}.txt    # Create multiple files
+touch test{1..3}       # Create test1, test2 and test3 files
+touch test{a..c}       # Create testa, testb and testc files
+
+mktemp                 # Create a temporary file
+
+echo "foo" > bar.txt       # Overwrite file with content
+echo "foo" >> bar.txt      # Append to file with content
+
+ls exists 1> stdout.txt    # Redirect the standard output to a file
+ls noexist 2> stderror.txt # Redirect the standard error output to a file
+ls 2>&1 > out.txt          # Redirect standard output and error to a file
+ls > /dev/null             # Discard standard output and error
+
+read foo                   # Read from standard input and write to the variable foo
+
+cat foo.txt            # Print all contents
+less foo.txt           # Print some contents at a time (g - go to top of file, SHIFT+g, go to bottom of file, /foo to search for 'foo')
+head foo.txt           # Print top 10 lines of file
+tail foo.txt           # Print bottom 10 lines of file
+open foo.txt           # Open file in the default editor
+wc foo.txt             # List number of lines words and characters in the file
